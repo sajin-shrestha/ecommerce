55961c00c5b0a477a0268b971721159b666efc53
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-05-20 06:25:51 +0545

    trying to fix bug on Makefile

--- Makefile
+++ Makefile
@@ -6,3 +6,13 @@ test:
 
 run: build
 	@./bin/ecommerce
+
+migration:
+	@migrate create -ext sql -dir cmd/migrate/migrations $(filter-out $@,$(MAKECMDGOALS))
+
+migrate-up:
+	@go run cmd/migrate/main.go up
+
+
+migrate-down:
+	@go run cmd/migrate/main.go down
--- cmd/migrate/main.go
+++ cmd/migrate/main.go
@@ -8,7 +8,6 @@ import (
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/mysql"
 
-	// "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/sajin-shrestha/ecommerce/config"
 	"github.com/sajin-shrestha/ecommerce/db"
 )
