18b213a7eacbe87ac209e0712e3a1bf09c20b853
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-05-25 22:21:42 +0545

    database mirations and completing createUser function in store.go

--- service/user/store.go
+++ service/user/store.go
@@ -61,12 +61,10 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	if err != nil {
 		return nil, err
 	}
-	defer rows.Close()
 
-	u := &types.User{} // Initialize u outside the loop
-
+	u := new(types.User)
 	for rows.Next() {
-		err := scanRowIntoUser(rows, u)
+		u, err = scanRowsIntoUser(rows)
 		if err != nil {
 			return nil, err
 		}
@@ -79,7 +77,9 @@ func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	return u, nil
 }
 
-func scanRowIntoUser(rows *sql.Rows, user *types.User) error {
+func scanRowsIntoUser(rows *sql.Rows) (*types.User, error) {
+	user := new(types.User)
+
 	err := rows.Scan(
 		&user.ID,
 		&user.FirstName,
@@ -89,16 +89,38 @@ func scanRowIntoUser(rows *sql.Rows, user *types.User) error {
 		&user.CreatedAt,
 	)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	return nil
+	return user, nil
 }
 
 func (s *Store) GetUserByID(id int) (*types.User, error) {
-	return nil, nil
+	rows, err := s.db.Query("SELECT * FROM users WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	u := new(types.User)
+	for rows.Next() {
+		u, err = scanRowsIntoUser(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if u.ID == 0 {
+		return nil, fmt.Errorf("user not found")
+	}
+
+	return u, nil
 }
 
 func (s *Store) CreateUser(user types.User) error {
+	_, err := s.db.Exec("INSERT INTO users (firstName, lastName, email, password) VALUES (?,?,?,?)", user.FirstName, user.LastName, user.Email, user.Password)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
