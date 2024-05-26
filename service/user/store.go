0d19f7d2aa62b6c5376471e6595077f15540608e
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-05-26 20:48:37 +0545

    removed unnecessary code and comments

--- service/user/store.go
+++ service/user/store.go
@@ -15,47 +15,6 @@ func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
 }
 
-// func (s *Store) GetUserByEmail(email string) (*types.User, error) {
-// 	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-// 	defer rows.Close()
-
-// 	u := new(types.User)
-
-// 	for rows.Next() {
-// 		u, err := scanRowIntoUser(rows)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 	}
-
-// 	if u.ID == 0 {
-// 		return nil, fmt.Errorf("user not found")
-// 	}
-
-// 	return u, nil
-// }
-
-// func scanRowIntoUser(rows *sql.Rows) (*types.User, error) {
-// 	user := new(types.User)
-// 	err := rows.Scan(
-// 		&user.ID,
-// 		&user.FirstName,
-// 		&user.LastName,
-// 		&user.Email,
-// 		&user.Password,
-// 		&user.CreatedAt,
-// 	)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	return user, nil
-// }
-
-// uncomment GetUserByEmail() and scanRowIntoUser() above if it throws an error and comment below code
 func (s *Store) GetUserByEmail(email string) (*types.User, error) {
 	rows, err := s.db.Query("SELECT * FROM users WHERE email = ?", email)
 	if err != nil {
