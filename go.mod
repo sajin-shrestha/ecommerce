20ea91afa3664d86baeec28b503c962cec2baa03
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-05-28 14:16:11 +0545

    jwt authorization

--- go.mod
+++ go.mod
@@ -23,6 +23,8 @@ require (
 require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/go-playground/validator/v10 v10.20.0
+	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/golang-jwt/jwt/v5 v5.2.1
 	github.com/golang-migrate/migrate/v4 v4.17.1
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.23.0
---
+++ service/auth/jwt.go
@@ -0,0 +1,19 @@
+package auth
+
+import (
+	"strconv"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func createJWT(secret []byte, userID int) (string, error) {
+	expiration := time.Second * time.Duration(config)
+	
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userID" : strconv.Itoa(userID),
+		"expiredAt" : time.Now().Add(expirat).Unix()
+	})
+
+}
