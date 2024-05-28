c4b9830e75e800efc47dc31dcf4fa1644529408d
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-05-28 15:11:09 +0545

    jwt and hashpassword tested

---
+++ service/auth/jwt_test.go
@@ -0,0 +1,16 @@
+package auth
+
+import "testing"
+
+func TestCreateJWT(t *testing.T) {
+	secret := []byte("secret")
+
+	token, err := CreateJWT(secret, 1)
+	if err != nil {
+		t.Errorf("error creating JWT: %v", err)
+	}
+
+	if token == "" {
+		t.Errorf("expected token to be not empty")
+	}
+}
