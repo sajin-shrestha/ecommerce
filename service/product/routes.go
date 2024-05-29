af92d889cae096ff66f410347d8b89adb1ca331a
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-05-29 16:12:48 +0545

    product basic handler defined

--- service/product/routes.go
+++ service/product/routes.go
@@ -1,1 +1,15 @@
 package product
+
+import "github.com/gorilla/mux"
+
+type Handler struct {
+
+}
+
+func NewHandler() *Handler {
+	return &Handler{}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	
+}
