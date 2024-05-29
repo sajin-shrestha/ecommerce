e45ec6fa74c706f3cd7d9b00146157c07d76990e
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-05-29 20:25:51 +0545

    created product service

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/sajin-shrestha/ecommerce/service/product"
 	"github.com/sajin-shrestha/ecommerce/service/user"
 )
 
@@ -28,6 +29,10 @@ func (s *APIServer) Run() error {
 	userStore := user.NewStore(s.db)
 	userHandler := user.NewHandler(userStore)
 	userHandler.RegisterRoutes(subrouter)
+
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore)
+	productHandler.RegisterRoutes(subrouter)
 
 	log.Println("Listening on:", s.addr)
 
--- service/product/routes.go
+++ service/product/routes.go
@@ -1,15 +1,31 @@
 package product
 
-import "github.com/gorilla/mux"
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sajin-shrestha/ecommerce/types"
+	"github.com/sajin-shrestha/ecommerce/utils"
+)
 
 type Handler struct {
-
+	store types.ProductStore
 }
 
-func NewHandler() *Handler {
-	return &Handler{}
+func NewHandler(store types.ProductStore) *Handler {
+	return &Handler{store: store}
 }
 
 func (h *Handler) RegisterRoutes(router *mux.Router) {
-	
+	router.HandleFunc("/products", h.handleCreateProduct).Methods(http.MethodGet)
+}
+
+func (h *Handler) handleCreateProduct(w http.ResponseWriter, r *http.Request) {
+	ps, err := h.store.GetProducts()
+	if err != nil {
+		utils.WriteError(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	utils.WriteJSON(w, http.StatusOK, ps)
 }
