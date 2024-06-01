4e154b3095c3c8108fed0820f63a9cdf7e57ded1
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-06-01 17:40:26 +0545

    cart and order

--- service/cart/routes.go
+++ service/cart/routes.go
@@ -1,5 +1,24 @@
 package cart
 
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sajin-shrestha/ecommerce/types"
+)
+
 type Handler struct {
-	store types.C
+	store types.OrderStore
+}
+
+func NewHandler(store types.OrderStore) *Handler {
+	return &Handler{store: store}
+}
+
+func (h *Handler) RegisterRoutes(router *mux.Router) {
+	router.HandleFunc("/cart/checkout", h.handleCheckout).Methods(http.MethodPost)
+}
+
+func (h *Handler) handleCheckout(w http.ResponseWriter, r *http.Request) {
+
 }
