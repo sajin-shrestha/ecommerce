293a9243983901da56aec4cc11fb58324bd5ee08
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-06-07 16:46:47 +0545

    createOrder store handler created

--- service/order/store.go
+++ service/order/store.go
@@ -1,6 +1,10 @@
 package cart
 
-import "database/sql"
+import (
+	"database/sql"
+
+	"github.com/sajin-shrestha/ecommerce/types"
+)
 
 type Store struct {
 	db *sql.DB
@@ -8,4 +12,18 @@ type Store struct {
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{db: db}
+}
+
+func (s *Store) CreateOrder(order types.Order) (int, error) {
+	res, err := s.db.Exec("INSERT INTO orders (userId, total, status, address) VALUES(?,?,?,?)", order.UserID, order.Total, order.Status, order.Address)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
