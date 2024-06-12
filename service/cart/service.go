42ef847929c19e2b436e6641b43a24add91db5de
SajinShrestha2003 <shresthasajin59@gmail.com>
2024-06-12 20:18:36 +0545

    create-order service created

--- service/cart/service.go
+++ service/cart/service.go
@@ -18,3 +18,17 @@ func getCartItemsIDs(items []types.CartItem) ([]int, error) {
 
 	return productIDs, nil
 }
+
+func (h *Handler) createOrder(ps []types.Product, item []types.CartItem, userID int) (int, float64, error) {
+	// we need to loop thorugh products many times so we create a product-map for ease of use and optimization
+	productMap := make(map[int]types.Product)
+	for _, product := range ps {
+		productMap[product.ID] = product
+	}
+
+	// check if all products are actually in stock
+	// calculate the total price
+	// reduce the quantity of products in our database
+	// create the order
+	// create order items
+}
